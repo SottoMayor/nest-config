a693f25e2a944a6e08cf5da571e487cefb52dcac
SottoMayor <ddsmmf@gmail.com>
2024-10-16 11:16:40 -0400

    Setting up the TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,33 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from './users/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
       ConfigModule.forRoot({
         envFilePath: './.env',
         isGlobal: true
+      }),
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory(configService: ConfigService) {
+          return {
+            type: 'postgres',
+            host: configService.get<string>('DB_HOST'),
+            port: parseInt(configService.get<string>('DB_PORT')),
+            username: configService.get<string>('DB_USER'),
+            password: configService.get<string>('DB_PASSWORD'),
+            database: configService.get<string>('DB_NAME'),
+            synchronize: false,
+            entities: [
+              User
+            ],
+          };
+        }
       }),
     UsersModule],
   controllers: [AppController],
