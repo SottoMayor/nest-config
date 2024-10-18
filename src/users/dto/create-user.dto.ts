c796e6204ea1065fe83ccb66fd2ab5877c1d269a
SottoMayor <ddsmmf@gmail.com>
2024-10-18 14:15:05 -0400

    Setting the validation pipe and validating DTOs

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsString, IsEnum, IsNumber, IsDateString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsDateString()
+  birthdate: Date;
+
+  @IsEnum(['M', 'F'])
+  gender: 'M' | 'F';
+
+  @IsNumber()
+  height: number;
+
+  @IsNumber()
+  weight: number;
+}
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -2,3 +2,6 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
+
+// Obs: Com essa abordagem UpdateUserDto herda todas as validações do CreateUserDto.
+// Todos os seus campos, de atualização, são opcionais
