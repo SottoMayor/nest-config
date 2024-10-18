d5ddefeaa48d2edcf857059e1483c5591d0d0082
SottoMayor <ddsmmf@gmail.com>
2024-10-18 12:41:31 -0400

    Creating the User migration

--- src/database/migrations/1729096144029-user.ts
+++ src/database/migrations/1729096144029-user.ts
@@ -1,11 +1,46 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class User1729096144029 implements MigrationInterface {
-
+export class CreateUserTable1697654321000 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'user',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                    },
+                    {
+                        name: 'birthdate',
+                        type: 'date',
+                    },
+                    {
+                        name: 'gender',
+                        type: 'enum',
+                        enum: ['M', 'F'],
+                    },
+                    {
+                        name: 'height',
+                        type: 'float',
+                    },
+                    {
+                        name: 'weight',
+                        type: 'float',
+                    },
+                ],
+            }),
+            true
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('user');
     }
-
 }
