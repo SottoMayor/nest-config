86037f2bb835504825759e413bdeb7d6e55eb654
SottoMayor <ddsmmf@gmail.com>
2025-01-10 14:23:56 -0400

    AuthMod config

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,26 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([User])]
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      async useFactory(configService: ConfigService) {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: 60 * 60 }, // validade do token de 1h
+        };
+      },
+    }),
+  ]
 })
 export class UsersModule {}
