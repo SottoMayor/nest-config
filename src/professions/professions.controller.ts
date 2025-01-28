1525d0d468411d57f87542d07a746050afe87283
SottoMayor <ddsmmf@gmail.com>
2025-01-28 16:36:25 -0400

    Auth in other modules

--- src/professions/professions.controller.ts
+++ src/professions/professions.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ProfessionsService } from './professions.service';
 import { AssociateDto } from './dto/associate.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('professions')
 export class ProfessionsController {
   constructor(private readonly professionsService: ProfessionsService) {}
 
+  @UseGuards(AuthGuard())
   @Post('/associate')
-  create(@Body() associateDto: AssociateDto) {
+  create(@Body() associateDto: AssociateDto, @GetUser() authUser: User) {
+    console.log(`\n[AuthUser]: \n${JSON.stringify(authUser)}\n`)
     return this.professionsService.associate(associateDto);
   }
 }
